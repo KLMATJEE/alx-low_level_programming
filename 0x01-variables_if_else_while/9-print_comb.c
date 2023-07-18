5c3db91cc7b752882c48cba2b552b4841e39e156
git.@github.com:KLMATJEE <git.@github.com:KLMATJEE>
2023-07-18 00:33:07 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       
+        int n;
+        
+        for (i = 48; n < 58; n++)
+        {       
+                putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
